2b8aa2e19a0ab6460c667c60e0c7f652ea712625
Ha Van Binh <havanbinh22022003@gmail.com>
2025-03-10 17:27:50 +0700

    Implement multi-languages with i18next

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.tsx'
+import I18nDemo from './samples/I18nDemo.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    
+    <I18nDemo />
   </StrictMode>,
 )
