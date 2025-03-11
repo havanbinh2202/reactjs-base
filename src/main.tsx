e92f26fa9385c9786f2fac39354bb158dc2c90fd
Ha Van Binh <havanbinh22022003@gmail.com>
2025-03-11 07:12:10 +0700

    Implement recoil

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 // import App from './App.tsx'
 import I18nDemo from './samples/I18nDemo.tsx';
+import { RecoilRoot } from "recoil";
+import RecoilDemo from './samples/RecoilDemo.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-    <I18nDemo />
+    <RecoilRoot>
+      <I18nDemo />
+      <RecoilDemo />
+    </RecoilRoot>
   </StrictMode>,
 )
